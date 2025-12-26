fc5454e20e73a545b2e3e6f120b886ff86cc6649
Saneef Ansari <hello@saneef.com>
2025-12-26 16:08:14 +0530

    Bumps to Elevent v3.x
    - Port the config to ESM from CJS

--- eleventy.config.cjs
+++
@@ -1,5 +0,0 @@
-const pluginGitCommitDate = require("eleventy-plugin-git-commit-date");
-
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(pluginGitCommitDate);
-};
---
+++ eleventy.config.js
@@ -0,0 +1,5 @@
+import pluginGitCommitDate from "eleventy-plugin-git-commit-date";
+
+export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginGitCommitDate);
+}
